1d3520ee0eec2f8e3635c5355d81487206cab873
unknown <carreondianaaa@gmail.com>
2024-03-28 19:16:57 +0800

    hahaha

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -25,7 +25,7 @@
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <a href="" class="text-white px-3 py-2 rounded-md text-sm font-medium" style="background-color: #A9B388">Home</a>
-                                <a href="" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About Us</a>
+                                <a href="" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ">About Us</a>
                                 <a href="" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact Us</a>
                             </div>
                         </div>
